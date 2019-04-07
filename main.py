38649167e3d85d666288a603c0bcb3e84ce2f5d8
GlennToms <Glenn@glenntoms.gg>
2019-04-07 19:06:38 +0100

    Adding a Pause to completed but not removed.

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def get_torrents():
     torrent_list = []
     for torrent in torrents:
         if torrent['amount_left'] == 0:
-            print(f"qBittorrent: Removing {torrent['name']}")
             torrent_list.append((torrent['name'], torrent['hash']))
     return torrent_list
 
@@ -40,10 +39,14 @@ def remove_complete_torrents(torrent_list, sonarr_list):
     for torrent in torrent_list:
         for history in sonarr_list:
             if torrent[0] in history:
-                print(f"Matched: {torrent[0]}")
+                print(f"Removing: {torrent[0]}")
                 remove_list.append(torrent[1])
 
-    qb.delete_permanently(remove_list)
+    #qb.delete_permanently(remove_list)
+
+def pause_complete():
+    torrents = get_torrents()
+    qb.pause_multiple(torrents[0][1])
 
 
 def get_sonarr_history():
@@ -55,7 +58,7 @@ def get_sonarr_history():
     hist = sonarr.get_history_size(100)
     for record in hist['records']:
         if record['data']['downloadClient'] == 'QBittorrent':
-            print(f"Sonarr: {record['data']['downloadClient']} - {record['sourceTitle']}")
+            # print(f"Sonarr: {record['data']['downloadClient']} - {record['sourceTitle']}")
             hist_list.append(record['sourceTitle'])
     return hist_list
 
@@ -63,4 +66,5 @@ if __name__ == '__main__':
     torrents = get_torrents()
     sonarr_list = get_sonarr_history()
     remove_complete_torrents(torrents, sonarr_list)
+    pause_complete()
     print("complete")
